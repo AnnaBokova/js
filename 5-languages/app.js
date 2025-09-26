f2d5ed6401ba92726b0ed4b3dfe1080f4cb02a38
AnnaBokova <anubokova@gmail.com>
2025-09-26 12:25:44 +0700

    Коррекция ДЗ согласно ревью

--- 5-languages/app.js
+++ 5-languages/app.js
@@ -1,21 +1,19 @@
 //Написать код, который при передаче пользователем языка: en, ru, de выводит
 // в консоль соответствующее приветствие на указанном языке
 
-let lang = prompt("Выберите язык: Русский, English, German");
-let ru = "Русский";
-let en = "English";
-let de = "German";
+let Lang = "de"; // Можно поменять на 'en', 'ru'
 
-switch (lang) {
-  case en:
+switch (Lang) {
+  case "en":
     console.log("Hello!");
     break;
-  case ru:
+  case "ru":
     console.log("Привет!");
     break;
-  case de:
-    console.log("Hallo!");
+  case "de":
+    console.log("Gutten tag!");
     break;
   default:
-    console.log("Вы не выбрали язык");
+    console.log("Вы не выбрали язык"); // Язык по умолчанию — русский
+    break;
 }
